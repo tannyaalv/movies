0902c59f2974790fdd49f97b0283deef82cea490
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-04-22 12:19:30 -0500

    Initial Commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Alvarez-Salsedo, Tanya on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
